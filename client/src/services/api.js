b65f13884991df01c37a5310ae178a8905270a65
Praveen Methraskar <55995179+praveenmethraskar@users.noreply.github.com>
2025-01-22 15:16:19 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || "http://43.205.136.242:3001", 
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://15.207.86.217:3001", 
 });
 
 
